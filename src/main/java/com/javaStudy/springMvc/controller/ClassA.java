aaca5b8e4714da85e39e0e4d890513254d506be2
mmikilchenko <testmarinatest@gmail.com>
2016-12-02 13:30:31 +0200

    - app context

--- src/main/java/com/javaStudy/springMvc/controller/ClassA.java
+++
@@ -1,8 +0,0 @@
-package com.javaStudy.springMvc.controller;
-
-/**
- * Created by mmikilchenko on 01.12.2016.
- */
-public class ClassA {
-    String test  = "Test3";
-}
---
+++ src/main/java/com/javaStudy/springMvc/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.javaStudy.springMvc.controller;
+
+/**
+ * Created by mmikilchenko on 02.12.2016.
+ */
+public class MainController {
+}
