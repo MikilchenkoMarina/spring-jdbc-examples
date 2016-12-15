e3f577bad85382d73d9764eb8db2f1268869bb87
mmikilchenko <testmarinatest@gmail.com>
2016-12-15 16:31:01 +0200

    - add SpringJDBC - example

---
+++ src/main/java/databasemy/databasemy.java
@@ -0,0 +1,8 @@
+package databasemy;
+
+/**
+ * Created by mmikilchenko on 14.12.2016.
+ */
+public class                                                                                                        databasemy {
+
+}
