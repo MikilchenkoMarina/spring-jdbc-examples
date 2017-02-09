9253d73b2deba93e0b25160c44d36fc750b23a32
Andreas Tarakanov <tarakanovandy@gmail.com>
2017-02-09 13:45:13 +0100

    inspo data base created

---
+++ src/main/java/inspoDataBase/ApplicationInspoDataBaseTest.java
@@ -0,0 +1,13 @@
+package inspoDataBase;
+
+import contactdatabase.dao.services.ContactDAO;
+import contactdatabase.model.Contact;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+public class ApplicationInspoDataBaseTest {
+}
