1ec02526fca0fac15b4af36abda0ce46fbd623cd
Marina <tarakanovandy@gmail.com>
2017-02-21 15:28:28 +0100

    6.2  Jpa data access is added via JpaRepository interface extending

--- src/main/java/inspoDataBase/jpaUsageDataBase/jpaRepository/UserRepository.java
+++ src/main/java/inspoDataBase/jpaUsageDataBase/jpaRepository/UserRepository.java
@@ -3,6 +3,8 @@ package inspoDataBase.jpaUsageDataBase.jpaRepository;
 
 import inspoDataBase.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,9 +12,16 @@ import java.util.List;
  * Created by mmikilchenko on 09.02.2017.
  */
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    // Demonstrate query creation by method name
+    // http://static.springsource.org/spring-data/data-jpa/docs/current/reference/html/#jpa.query-methods.query-creation
     User saveAndFlush(User user);
 
     List<User> findAll();
+
+    // Demonstrate the use of a simple JPQL query
+    @Query("from User u where upper(u.user_name) = upper(:userName)")
+    User findByNameIgnoreCase(@Param("userName") String userName);
 
 
 }
